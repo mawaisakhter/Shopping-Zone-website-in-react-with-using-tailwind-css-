985f1c1f3b3a3340a160b1e7bbf030af5b7dc4d6
Awais <159285589+mawaisakhter@users.noreply.github.com>
2024-08-30 12:49:22 +0500

    Update headnav.jsx

--- src/compoents/navbar/headnav.jsx
+++ src/compoents/navbar/headnav.jsx
@@ -6,7 +6,7 @@ const Headnav = () => {
     return (
         <>
             <nav className="bg-white text-black border z-20 fixed top-0 left-0 right-0 px-4 flex items-center justify-between"><FaEnvelope className='md:ms-20 hidden md:visible'/>
-                <Link to="/" className="px-2 border-r-2 ">support@shoppingstore.com</Link>
+                <Link to="/" className="px-2 border-r-2 hidden md:block">support@shoppingstore.com</Link>
                 <FaPhone className='ml-2'/>
                 <h1 className='mr-auto border-r-2 px-2'>+923001262260</h1>
                 <div className="flex items-center">
